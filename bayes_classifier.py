dba4d20b81184b9b0377619a27ebebbdf82304a3
Leopold Yang <yzs19990810@gmail.com>
2022-04-03 11:56:27 +0800

    modify files.

--- bayes_classifier.py
+++ bayes_classifier.py
@@ -14,20 +14,30 @@ class BayesClassifier:
         priori_proba = np.sum(priori_idx, 0) / self.y_train.shape[0]
         return priori_proba
 
-    def _calc_conditional_priori_dist(self):
+    def _calc_conditional_priori_dist_param(self):
+        # multivariate normal density function
         cpd = []
         for i in range(self.classes.shape[0]):
-            x_mean = np.mean(self.x_train[np.where(self.y_train == self.classes[i])[0]], 0)
-            x_var = np.std(self.x_train[np.where(self.y_train == self.classes[i])[0]], 0)
-            cpd.append(norm(x_mean, x_var))
+            x_data = self.x_train[np.where(self.y_train == self.classes[i])[0]]
+            x_mean = np.mean(x_data, 0)
+            x_cov = np.cov(x_data.T)
+            cpd.append((x_mean, x_cov))
         return cpd
 
     def classify(self):
         priori_proba = self._calc_priori_proba()
-        cpd = self._calc_conditional_priori_dist()
-        post_proba = np.zeros((self.x_test.shape[0], self.classes.shape[0]))
-        for i in range(self.classes.shape[0]):
-            post_proba[:, i] = priori_proba[i] * np.prod(cpd[i].pdf(self.x_test), 1)
+        cpd = self._calc_conditional_priori_dist_param()
+        c_n = self.classes.shape[0]
+        x_n = self.x_train.shape[0]
+        post_proba = np.zeros((x_n, c_n))
+        for xt in range(x_n):
+            for i in range(c_n):
+                xm = cpd[i][0]
+                xc = cpd[i][1]
+                post_proba[xt, i] = priori_proba[i] \
+                                    * (1 / (((2 * np.pi) ** 2) * np.linalg.det(xc) ** 0.5)) \
+                                    * np.exp(np.dot(np.dot(-0.5 * (self.x_train[xt, :] - xm).T, np.linalg.inv(xc)),
+                                                    (self.x_train[xt, :] - xm)))
+        # print(post_proba)
         res = self.classes[np.argmax(post_proba, 1)]
         return res
-
